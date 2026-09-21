d81bbbc84af92049d39007945612329f388b7313
ziyue67 <2094758526@qq.com>
2026-09-21 23:30:59 +0800

    fix(linux): port Wayland portal response handling

--- src/error.rs
+++ src/error.rs
@@ -31,6 +31,9 @@ pub enum XCapError {
     StdIOError(#[from] std::io::Error),
     #[cfg(target_os = "linux")]
     #[error(transparent)]
+    StdMPSCRecvError(#[from] std::sync::mpsc::RecvError),
+    #[cfg(target_os = "linux")]
+    #[error(transparent)]
     StdTimeSystemTimeError(#[from] std::time::SystemTimeError),
     #[cfg(target_os = "linux")]
     #[error(transparent)]
